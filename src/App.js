882077dc33270c708b4a225896a7eddb1ae45067
knight <478274029@qq.com>
2019-06-29 21:08:55 +0800

    router finished

--- src/App.js
+++ src/App.js
@@ -27,3 +27,5 @@ class App extends Component {
   }
 }
 
+export default App;
+
--- src/layouts/DefaultLayout/BlankLayout.js
+++ src/layouts/DefaultLayout/BlankLayout.js
@@ -1,42 +1,65 @@
 import React, {Component, component} from 'react';
 import Icon from "antd/es/icon";
-import {Layout} from 'antd'
+import './login.less'
+import myLogo from '../../assests/mlogo.svg'
+import {Menu} from "antd";
+import Dropdown from "antd/es/dropdown";
 
-const {Header, Content, Footer} = Layout
-
-const Ghead = (
-    <Header className='blank-header'>
-        <Icon type="global" />
-    </Header>
+const menu = (
+    <Menu>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.baidu.com/">
+                1st menu item
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.baidu.com/">
+                2nd menu item
+            </a>
+        </Menu.Item>
+        <Menu.Item>
+            <a target="_blank" rel="noopener noreferrer" href="http://www.baidu.com/">
+                3rd menu item
+            </a>
+        </Menu.Item>
+    </Menu>
 )
-
-const Gfoot = (
-
-    <Footer className='blank-footer'>
-        <div>
-        <img alt='logo' src='../../assests/mlogo.svg'/>
-        </div>
-        <div className='blank-footer-copyright'>
-            你比五环多一环     花旗杯创新项目
-        </div>
-    </Footer>
-)
-
 
 class BlankLayout extends Component{
 
     render() {
         const {children} = this.props;
         return (
-            <main>
-                <div>
-                    <Ghead/>
-                    <Content>{children}</Content>
-                    <Gfoot/>
+            <layout>
+                <div className="container">
+                    <header>
+                        <Dropdown overlay={menu}>
+                            <Icon type="global" className='icon'/>
+                        </Dropdown>
+                    </header>
+                    <div className='content'>
+                        <div className='slogan'>
+                            欢迎来到登陆页面
+                        </div>
+                        <div>
+                            <div style={{display: "inline-block", width: "23%"}}></div>
+                            <div style={{display: "inline-block"}}>
+                                {children}
+                            </div>
+                            <div style={{display: "inline-block", width: "23%"}}></div>
+                        </div>
+                        <footer>
+                            <div>
+                                你比五环多一环
+                                <img alt='logo' className='pic' src={myLogo}/>
+                                花旗杯创新项目
+                            </div>
+                        </footer>
+                    </div>
                 </div>
-            </main>
+            </layout>
         )
     }
 }
 
-export default BlankLayout;
+    export default BlankLayout;
