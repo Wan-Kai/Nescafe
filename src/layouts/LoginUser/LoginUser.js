ac114fd328583ea9d17c1bb14cdc514f80f5cdd7
Wan-Kai <39261115+Wan-Kai@users.noreply.github.com>
2019-07-02 11:56:25 +0800

    style changed

--- src/layouts/LoginUser/LoginUser.js
+++ src/layouts/LoginUser/LoginUser.js
@@ -1,6 +1,7 @@
 import React, {Component, component} from 'react';
 import {Layout,Menu,Icon,SubMenu,Dropdown} from 'antd';
 import './LoginUser.less';
+import mlogo from "../../assets/img/mlogo.png";
 
 const {Header,Footer,Content} = Layout;
 
@@ -26,31 +27,37 @@ export default class LoginUser extends Component{
         const { children } = this.props;
         return (
             <Layout>
-                <Header>
-                    <div  />
+                <Header style={{ background: '#FFFFFF', padding: 0}} className="header">
+
+
                     <Menu
-                        theme="dark"
+                        theme="light"
                         mode="horizontal"
                         defaultSelectedKeys={['1']}
-                        style={{ lineHeight: '64px' }}
+                        style={{ lineHeight: '60px'}}
                     >
+
                         <Menu.Item key="1">企业本页</Menu.Item>
                         <Menu.Item key="2">信息</Menu.Item>
                         <Menu.Item key="3">设置</Menu.Item>
 
+                        <img  className="logo" src={mlogo}/>
                         <Dropdown overlay={menu}>
                             <a className="global" href="#">
                                 语言 <Icon type="global" />
                             </a>
                         </Dropdown>
-
-
                     </Menu>
                 </Header>
-                     <Content>
-                        {children}
-                     </Content>
-                <Footer className="footer">Copyright by Wan</Footer>
+                <div >
+                    <layout>
+                        <Content>
+                            {children}
+                            <Footer className="footer">Copyright by Wan</Footer>
+                        </Content>
+                    </layout>
+                </div>
+
             </Layout>
 
         )
