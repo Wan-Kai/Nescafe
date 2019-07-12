9b4d74aaedca6810753775b94db41103d0cd613b
Wan-Kai <39261115+Wan-Kai@users.noreply.github.com>
2019-07-12 16:22:05 +0800

    link

--- src/pages/UserCenter/AccountPage/AccountPage.js
+++ src/pages/UserCenter/AccountPage/AccountPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Progress} from 'antd';
 import myLogo from "../../../assets/img/mlogo.png";
 import './AccountPage.less'
 
@@ -10,19 +11,30 @@ class AccountPage extends React.Component{
         return(
             <div className="user-center-account-main" style={{display:"flex"}}>
                 <div className="user-center-account-table" style={{width:'-webkit-fill-available'}}>
-                    <h2>安全设置</h2>
+                    <h2>基本列表</h2>
                     <div className="user-center-account-table-item">
                         <div className="user-center-account-table-text">
                             <div className="user-center-account-table-text-item">
                                 <img src={myLogo} style={{height:50,width:50,marginBottom:5}}/>
                             </div>
-                            <div className="user-center-account-table-text-item">
+                            <div className="user-center-account-table-text-item" style={{width:200}}>
                                 <p>账号密码</p>
                                 <p>你的************</p>
                             </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>owner</p>
+                                <p>Lee</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>开始时间</p>
+                                <p>2019.7.12</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <Progress percent={30} size="small" />
+                            </div>
                         </div>
                         <div className="user-center-account-table-href">
-                            <a>修改</a>
+                            <a href="#/user/detail" target='_self'>查看</a>
                         </div>
                     </div>
                     <div className="user-center-account-table-item">
@@ -30,13 +42,24 @@ class AccountPage extends React.Component{
                             <div className="user-center-account-table-text-item">
                                 <img src={myLogo} style={{height:50,width:50,marginBottom:5}}/>
                             </div>
-                            <div className="user-center-account-table-text-item">
+                            <div className="user-center-account-table-text-item" style={{width:200}}>
                                 <p>账号密码</p>
                                 <p>你的************</p>
                             </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>owner</p>
+                                <p>Lee</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>开始时间</p>
+                                <p>2019.7.12</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <Progress percent={30} size="small" />
+                            </div>
                         </div>
                         <div className="user-center-account-table-href">
-                            <a>修改</a>
+                            <a href="#/user/detail" target='_self'>查看</a>
                         </div>
                     </div>
                     <div className="user-center-account-table-item">
@@ -44,13 +67,24 @@ class AccountPage extends React.Component{
                             <div className="user-center-account-table-text-item">
                                 <img src={myLogo} style={{height:50,width:50,marginBottom:5}}/>
                             </div>
-                            <div className="user-center-account-table-text-item">
+                            <div className="user-center-account-table-text-item" style={{width:200}}>
                                 <p>账号密码</p>
                                 <p>你的************</p>
                             </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>owner</p>
+                                <p>Lee</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>开始时间</p>
+                                <p>2019.7.12</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <Progress percent={30} size="small" />
+                            </div>
                         </div>
                         <div className="user-center-account-table-href">
-                            <a>修改</a>
+                            <a href="#/user/detail" target='_self'>查看</a>
                         </div>
                     </div>
                     <div className="user-center-account-table-item">
@@ -58,13 +92,24 @@ class AccountPage extends React.Component{
                             <div className="user-center-account-table-text-item">
                                 <img src={myLogo} style={{height:50,width:50,marginBottom:5}}/>
                             </div>
-                            <div className="user-center-account-table-text-item">
+                            <div className="user-center-account-table-text-item" style={{width:200}}>
                                 <p>账号密码</p>
                                 <p>你的************</p>
                             </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>owner</p>
+                                <p>Lee</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>开始时间</p>
+                                <p>2019.7.12</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <Progress percent={30} size="small" />
+                            </div>
                         </div>
                         <div className="user-center-account-table-href">
-                            <a>修改</a>
+                            <a href="#/user/detail" target='_self'>查看</a>
                         </div>
                     </div>
                     <div className="user-center-account-table-item">
@@ -72,13 +117,24 @@ class AccountPage extends React.Component{
                             <div className="user-center-account-table-text-item">
                                 <img src={myLogo} style={{height:50,width:50,marginBottom:5}}/>
                             </div>
-                            <div className="user-center-account-table-text-item">
+                            <div className="user-center-account-table-text-item" style={{width:200}}>
                                 <p>账号密码</p>
                                 <p>你的************</p>
                             </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>owner</p>
+                                <p>Lee</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <p>开始时间</p>
+                                <p>2019.7.12</p>
+                            </div>
+                            <div className="user-center-account-table-text-item" style={{width:100}}>
+                                <Progress percent={30} size="small" />
+                            </div>
                         </div>
                         <div className="user-center-account-table-href">
-                            <a>修改</a>
+                            <a href="#/user/detail" target='_self'>查看</a>
                         </div>
                     </div>
                 </div>
